5d73e375f1bb0579b94e8ac987e9f28ce52afdef
aaronjan98 <aaronjan98@gmail.com>
2021-04-15 13:54:39 -0700

    added user-agent test to bypass bot detection systems

--- indeed.js
+++ indeed.js
@@ -31,11 +31,66 @@ async function configureBrowser() {
         console.error({ error })
     })
 
+    await puppeteer.defaultArgs({
+        headless: true
+    })
     const websocket_connection = await puppeteer.connect({
-        browserWSEndpoint: webChromeEndpointUrl
+        browserWSEndpoint: webChromeEndpointUrl,
+        slowMo: 10
     })
 
     return websocket_connection.newPage()
+}
+
+// Bypass the bot detection systems
+const preparePageForTests = async (page) => {
+  // Pass the User-Agent Test.
+  const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+  await page.setUserAgent(userAgent)
+
+  // Pass the Webdriver Test.
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => false,
+    })
+  })
+
+  // Pass the Chrome Test.
+  await page.evaluateOnNewDocument(() => {
+    // We can mock this in as much depth as we need for the test.
+    window.chrome = {
+      runtime: {},
+      // etc.
+    }
+  })
+
+  // Pass the Permissions Test.
+  await page.evaluateOnNewDocument(() => {
+    const originalQuery = window.navigator.permissions.query;
+    return window.navigator.permissions.query = (parameters) => (
+      parameters.name === 'notifications' ?
+        Promise.resolve({ state: Notification.permission }) :
+        originalQuery(parameters)
+    )
+  })
+
+  // Pass the Plugins Length Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'plugins', {
+      // This just needs to have `length > 0` for the current test,
+      // but we could mock the plugins too if necessary.
+      get: () => [1, 2, 3, 4, 5],
+    })
+  })
+
+  // Pass the Languages Test.
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'languages', {
+      get: () => ['en-US', 'en'],
+    })
+  })
 }
 
 ;(async () => {
@@ -47,6 +102,7 @@ async function configureBrowser() {
     })
 
     const page = await configureBrowser()
+    await preparePageForTests(page)
 
     // scraping indeed for jobs
     try {
@@ -80,21 +136,36 @@ async function configureBrowser() {
             await page.waitForSelector('iframe#vjs-container-iframe')
             const iframeElement = await page.$('iframe#vjs-container-iframe')
             const frame = await iframeElement.contentFrame()
-            await frame.waitForSelector('#viewJobSSRRoot > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-JobInfoHeader-title-container.jobsearch-JobInfoHeader-title-containerEji > h1')
+            //const frame = await iframeElement.contentWindow()
+            //console.log({ frame })
+            //console.log(util.inspect(frame).contentDocument)
+            /*
+             Running this in the DOM works:
+             document.querySelector('iframe#vjs-container-iframe').contentDocument.querySelector('#viewJobSSRRoot > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-JobInfoHeader-title-container.jobsearch-JobInfoHeader-title-containerEji > h1').innerText
+            */
+            //const aHandle = await page.evaluateHandle(() => document.querySelector('iframe#vjs-container-iframe').contentDocument)
+            //const resultHandle = await page.evaluateHandle(body => body.querySelector('#viewJobSSRRoot').innerHTML, aHandle)
+            //console.log(await resultHandle.jsonValue());
+            //await resultHandle.dispose();
+            //const h1_title = await frame.$('#viewJobSSRRoot > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-JobInfoHeader-title-container.jobsearch-JobInfoHeader-title-containerEji > h1')
+            const h1_title = await frame.$('body')
+            console.log({ h1_title })
+            /*
+            const job_header = await page.evaluateHandle(async iframeElement => {
+                console.log('iframe doc: ', await iframeElement.contentDocument)
+                console.log('document.iframe: ', await document.querySelector('iframe#vjs-container-iframe').contentDocument)
+                console.log('src: ', await document.querySelector('iframe#vjs-container-iframe').contentDocument.src)
 
-            //console.log(util.inspect(frame))
-
-            const job_header = await page.evaluate(async frame => {
-                let foo = await frame.querySelector('#viewJobSSRRoot > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-JobInfoHeader-title-container.jobsearch-JobInfoHeader-title-containerEji > h1').innerText
+                let foo = await document.querySelector('iframe#vjs-container-iframe').contentDocument.querySelector('#viewJobSSRRoot > div > div.jobsearch-JobComponent-embeddedHeader > div > div:nth-child(2) > div.jobsearch-JobInfoHeader-title-container.jobsearch-JobInfoHeader-title-containerEji > h1').innerText
                 console.log({ foo })
-            }, frame)
-
+                return foo
+            }, iframeElement)
             console.log({ job_header })
+            */
         }
 
     } catch (error) {
         console.error(error)
     }
-
     //await browser_websocket_up.close()
 })()
